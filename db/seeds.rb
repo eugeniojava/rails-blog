2cb2c264b6d5f5183c5351eb7998a445d7bc33f3
Eugênio Moreira <me@eugeniomoreira.com>
2023-06-08 21:55:04 -0300

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+user = User.where(email: "me@eugeniomoreira.com").first_or_initialize
+user.update(password: "eugenio", password_confirmation: "eugenio")
